35edcf52e99cb71c0a899c7ea7f56c5b81bdfbce
Mike <mkalligas1997@gmail.com>
2026-07-09 00:17:42 +0300

    Back up existing home files on activation

--- flake.nix
+++ flake.nix
@@ -37,6 +37,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "before-nix";
             home-manager.extraSpecialArgs = {
               inherit user;
             };
